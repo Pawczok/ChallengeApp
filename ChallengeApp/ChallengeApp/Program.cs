0812f31f80d46f67d4255d9efedbd8ad242dd39d
Paweł Trajdos <pawczok@gmail.com>
2023-02-15 21:36:43 +0100

    Zadanie dzien 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+bool isWoman = false;
+int myAge = 80;
+
+
+if (isWoman == true && myAge < 30)
+{
+    Console.WriteLine("Women under 30 years old");
+}
+else if (name == "Ewa" && myAge == 33 && isWoman == true)
+{
+    Console.WriteLine("Ewa 33 years old");
+}
+else if (isWoman == false && myAge <18)
+{
+    Console.WriteLine("Male under 18 years old");
+}
+else 
+{
+    Console.WriteLine("Retirement date");
+}
+
